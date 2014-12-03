9e2a6de8d7e69a2fb2d8f3ae6e42365933e022db
wangh <303792351@qq.com>
2014-12-03 21:27:13 +0800

    201412032127

---
+++ 自绘标题栏/自绘标题栏.cpp
@@ -0,0 +1,87 @@
+/*
+duilib没有区分标题栏和客户区，用客户区模拟标题栏
+1 屏蔽系统自带标题栏
+	在HandleMessage里屏蔽 WM_NCACTIVATE | WM_NCCALCSIZE | WM_NCPAINT 三个消息
+*/
+
+
+/*
+1 调用AddNotifier将消息加入duilib的消息循环
+2 给按钮设置唯一的控件ID (SetName函数）
+3 在Notify函数里处理按钮点击消息
+*/
+
+
+#include <UIlib.h>
+using namespace DuiLib;
+
+class DuiFrameWnd :public CWindowWnd, public INotifyUI
+{
+public:
+	virtual LPCTSTR GetWindowClassName() const { return _T("DuiMainFrame"); }
+
+	// 处理消息
+	virtual void Notify(TNotifyUI& msg)
+	{
+		if (msg.sType == _T("click"))
+		{
+			if (msg.pSender->GetName() == _T("btnHello"))
+			{
+				::MessageBox(NULL, _T("我是按钮"), _T("点击了按钮"), NULL);
+			}
+		}
+	}
+
+	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		LRESULT lRes = 0;
+		if (uMsg == WM_CREATE)
+		{
+			// CControlUI 是所有控件的基类
+			CControlUI *pWnd = new CButtonUI;
+			pWnd->SetName(_T("btnHello")); // 设置控件的名字，唯一标识
+			pWnd->SetText(_T("HelloWorld")); // 设置文字
+			pWnd->SetBkColor(0xFF00FF00);
+			m_PaintManager.Init(m_hWnd);
+			m_PaintManager.AttachDialog(pWnd);
+			m_PaintManager.AddNotifier(this); // 添加控件消息响应
+			return lRes;
+		}
+		else if (uMsg == WM_NCACTIVATE)
+		{
+			if (!::IsIconic(m_hWnd))
+			{
+				return (wParam == 0) ? TRUE : FALSE;
+			}
+		}
+		else if (uMsg == WM_NCCALCSIZE)
+		{
+			return 0;
+		}
+		else if (uMsg == WM_NCPAINT)
+		{
+			return 0;
+		}
+
+		if (m_PaintManager.MessageHandler(uMsg, wParam, lParam, lRes))
+		{
+			return lRes;
+		}
+
+		return __super::HandleMessage(uMsg, wParam, lParam);
+	}
+
+protected:
+	CPaintManagerUI m_PaintManager;
+};
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	CPaintManagerUI::SetInstance(hInstance);
+
+	DuiFrameWnd duiFrame;
+	duiFrame.Create(nullptr, _T("DuiWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.ShowModal();
+
+	return 0;
+}
