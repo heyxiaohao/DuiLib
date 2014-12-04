835c9a30ca259cfe62be8311ad99dcec0fe6df6e
wangh <hey-xiaohao@163.com>
2014-12-04 11:17:04 +0800

    201412041116

--- 简单控件/简单控件.cpp
+++ 简单控件/简单控件.cpp
@@ -1,3 +1,11 @@
+
+/*
+可定制默认样式，如Button 如果没有指定属性，则使用默认属性
+给<Window>节点添加一个Default节点即可，其中
+name属性填写控件的名字，
+value属性添加控件的属性的值，需要将双引号【"】换成【&quot;】，单引号【'】换成【&apos;】,单引号也可以不转换。
+*/
+
 #include <UIlib.h>
 using namespace DuiLib;
 
@@ -6,7 +14,7 @@ class DuiFrameWnd : public WindowImplBase
 public:
 	virtual LPCTSTR GetWindowClassName() const { return _T("DUIMainFrame"); }
 	virtual CDuiString GetSkinFile() { return _T("duilib.xml"); }
-	virtual CDuiString GetSkinFolder() { return _T("../Bug"); }
+	virtual CDuiString GetSkinFolder() { return _T("../简单控件"); }
 };
 
 
