9ea0851ec3a2dbbdbb4ad4619ab6361fdcf6017d
wangh <hey-xiaohao@163.com>
2014-12-04 18:03:53 +0800

    201412041803

---
+++ 使用MFC控件/WndUI.h
@@ -0,0 +1,45 @@
+#ifndef __WND_UI_H__
+#define __WND_UI_H__
+
+#include <UIlib.h>
+using namespace DuiLib;
+
+class CWndUI : public CControlUI
+{
+public:
+	CWndUI() : m_hWnd(NULL) {}
+	virtual void SetInternVisible(bool bVisible = true)
+	{
+		__super::SetInternVisible(bVisible);
+		::ShowWindow(m_hWnd, bVisible);
+	}
+
+	virtual void SetPos(RECT rc)
+	{
+		__super::SetPos(rc);
+		::SetWindowPos(m_hWnd, NULL, rc.left, rc.top, rc.right - rc.left, rc.bottom - rc.top, SWP_NOZORDER | SWP_NOACTIVATE);
+	}
+
+	BOOL Attach(HWND hWndNew)
+	{
+		if (!::IsWindow(hWndNew))
+		{
+			return FALSE;
+		}
+
+		m_hWnd = hWndNew;
+		return TRUE;
+	}
+
+	HWND Detach()
+	{
+		HWND hWnd = m_hWnd;
+		m_hWnd = NULL;
+		return hWnd;
+	}
+
+protected:
+	HWND m_hWnd;
+};
+
+#endif // __WND_UI_H__
